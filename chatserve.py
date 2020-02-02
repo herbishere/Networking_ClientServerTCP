1f48bbeb8d5baa172ec5db14cd812f45325e3aaa
herbishere <hrbrtdiaz@gmail.com>
2020-02-02 16:50:07 -0600

    implemented starting code for server

--- chatserve.py
+++ chatserve.py
@@ -1,8 +1,31 @@
 from sys import argv
+from socket import *
 
-SERVER_HOST, SERVER_PORT = "localhost", 12000
+SERVER_PORT = 31392
+HOST_NAME = "HOST>"
 if len(argv) > 1:
     SERVER_PORT = argv[1]
 
 if __name__ == "__main__":
-    print(SERVER_PORT)
+    # Create TCP Socket
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('', SERVER_PORT))
+    # Listen for TCP Requests
+    serverSocket.listen(1)
+    print('Server Ready to Receive')
+    while 1:
+        connectionSocket, addr = serverSocket.accept()
+        while 1:
+            # Get Message from Client
+            clientMessage = connectionSocket.recv(500)
+            print(clientMessage)
+            # Get Message from Command Line
+            serverMessage = input(HOST_NAME)
+            # Send Message to Client if Quit Command not input
+            if serverMessage != "\\quit":
+                serverMessage = HOST_NAME + serverMessage
+                connectionSocket.send(serverMessage)
+            # Otherwise Close the Connection
+            else:
+                break
+        connectionSocket.close()
