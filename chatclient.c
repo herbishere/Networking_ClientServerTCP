512da8f4094d7246350e986f89f009586a124dd4
herbishere <hrbrtdiaz@gmail.com>
2020-02-02 22:33:43 -0600

    put starting code for client

---
+++ chatclient.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#define MAX_CHARACTERS 500
+
+int main(int argc, char *argv[])
+{
+    int socketFD, portNumber, charsWritten, charsRead;
+    struct sockaddr_in serverAddress;
+    struct hostent* serverHostInfo;
+    char buffer[MAX_CHARACTERS];
+
+    if (argc < 3) { fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); exit(0); } // Check usage & args
+
+    return 0;
+}
