d2af7cf1b8e4fde4dc82f98fab3e3b04eeb19166
herbishere <hrbrtdiaz@gmail.com>
2020-02-05 13:39:57 -0600

    Added header blocks

--- chatclient.c
+++ chatclient.c
@@ -1,3 +1,17 @@
+/**
+ * Programmer Name: Herbert Diaz <diazh@oregonstate.edu>
+ * Program Name:    Program 1 - Chat Client
+ * Program Description:
+ *      Usage: chatclient [serverhost] [serverport]
+ *      This program serves as the client for a messaging system
+ *      between a client and server using the TCP protocol. After
+ *      getting the user's handle, the user can send messages
+ *      until either they or the host terminate the connection with
+ *      the "\quit" command.
+ * Course Name:     CS 372 Intro to Computer Networks
+ * Last Modified:   02/05/2020
+**/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,6 +22,7 @@
 #include <netdb.h> 
 
 #define MAX_CHARACTERS 500
+#define HANDLE_SIZE 11
 
 int main(int argc, char *argv[])
 {
--- chatserve.py
+++ chatserve.py
@@ -1,3 +1,18 @@
+'''
+    Programmer Name: Herbert Diaz <diazh@oregonstate.edu>
+    Program Name:    Program 1 - Chat Server
+    Program Description:
+        Usage: chatserve [portnum]
+        This program serves as the server for a messaging system
+        between a client and server using the TCP protocol. After
+        connecting to and receiving a message from the user, the
+        host can send messages until either they or the client
+        terminates the connection with the "\quit" command. To
+        terminate the server, send a SIGINT with ctrl + c in the
+        terminal.
+    Course Name:     CS 372 Intro to Computer Networks
+    Last Modified:   02/05/2020
+'''
 from sys import argv
 from socket import socket, AF_INET, SOCK_STREAM
 
