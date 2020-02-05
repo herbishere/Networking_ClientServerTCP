83ff0dfdb0afafd35f96c94b47c9d674b5368df7
herbishere <hrbrtdiaz@gmail.com>
2020-02-05 16:38:47 -0600

    Made simpler, updated header

--- chatserve.py
+++ chatserve.py
@@ -7,21 +7,22 @@
         between a client and server using the TCP protocol. After
         connecting to and receiving a message from the user, the
         host can send messages until either they or the client
-        terminates the connection with the "\quit" command. To
-        terminate the server, send a SIGINT with ctrl + c in the
-        terminal.
+        terminates the connection with the '\\quit' (read: 
+        backslash-quit) (b) command. To terminate the server, 
+        send a SIGINT with ctrl + c in the terminal.
     Course Name:     CS 372 Intro to Computer Networks
     Last Modified:   02/05/2020
 '''
+
 from sys import argv
 from socket import socket, AF_INET, SOCK_STREAM
 
-MAX_CHARACTERS = 500
-SERVER_PORT = 31392
+MAX_CHARACTERS = 501
+SERVER_PORT = 7777
 HOST_NAME = "HOST>"
 if len(argv) > 1:
-    SERVER_PORT = argv[1]
-
+    SERVER_PORT = int(argv[1])
+    
 if __name__ == "__main__":
     # Create TCP Socket
     serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -32,17 +33,13 @@ if __name__ == "__main__":
     while 1:
         # Get
         connectionSocket, addr = serverSocket.accept()
-        while 1:
-            # Get Message from Client
-            clientMessage = connectionSocket.recv(MAX_CHARACTERS)
-            print(clientMessage)
-            # Get Message from Command Line
-            serverMessage = input(HOST_NAME)
-            # Send Message to Client if Quit Command not input
-            if serverMessage != "\\quit":
-                serverMessage = HOST_NAME + serverMessage
-                connectionSocket.send(serverMessage)
-            # Otherwise Close the Connection
-            else:
-                break
+        # Get Message from Client
+        clientMessage = connectionSocket.recv(MAX_CHARACTERS)
+        print(clientMessage)
+        # Get Message from Command Line
+        serverMessage = input(HOST_NAME)
+        # Send Message to Client if Quit Command not input
+        if serverMessage != "\\quit":
+            serverMessage = HOST_NAME + serverMessage
+            connectionSocket.send(serverMessage)
         connectionSocket.close()
